095d8c85db498b956bd70306638ade3662e54488
Student <example@example.com>
2019-11-12 15:24:54 -0800

    add object and prototype

--- src/journal.js
+++ src/journal.js
@@ -1,16 +1,24 @@
-// export function journal(subject, body) {
-//   let fullJournal = [];
-//   fullJournal.push(subject+body);
-//   return fullJournal;
-// };
-
-function Journal(subject, body) {
+export function Journal(subject, body) {
   this.subject = subject,
   this.body = body
-  let fullJournal = [];
 }
+let fullJournal = [];
 
 Journal.prototype.combineJournal = function() {
   fullJournal.push(subject+body);
   return fullJournal;
+  console.log(fullJournal);
 }
+
+Journal.prototype.getWords = function () {
+  return fullJournal = fullJournal.split(" ").length;
+  console.log(getWords);
+}
+
+
+
+// export function journal(subject, body) {
+  //   let fullJournal = [];
+  //   fullJournal.push(subject+body);
+  //   return fullJournal;
+  // };
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import { Journal } from './journal.js';
 $(document).ready(function() {
   $('#journalInput').submit(function(event) {
     event.preventDefault();
+    let fullJournal = [];
     let subject = $('#subjectInput').val();
     let body = $('#bodyInput').val();
-    myJournal = new Journal(subject, body)
+    let myJournal = new Journal(subject, body)
+    console.log(myJournal);
+    fullJournal.combineJournal(myJournal)
+    console.log(myJournal);
     });
 });
